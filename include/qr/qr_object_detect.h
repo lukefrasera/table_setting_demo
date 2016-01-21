8be033430337a9bbf443cb5b8386f7399f70cc8d
Luke Fraser <lukefrasera@gmail.com>
2016-01-21 13:36:17 -0800

    adding more class structure to the qr_detection methods

--- include/qr/qr_object_detect.h
+++ include/qr/qr_object_detect.h
@@ -2,17 +2,23 @@
 #define QR_OBJECT_DETECT_H_
 #include "log.h"
 #include "qr_detect.h"
+#include <opencv2/features2d.hpp>
+#include <opencv2/opencv.hpp>
 
 namespace qr {
 class Kalman2DTracker {
  public:
   Kalman2DTracker();
   virtual ~Kalman2DTracker();
+ private:
+  cv::KalmanFilter position_filter;
+  cv::KalmanFilter bounding_filter;
 };
 
 class QrFeatureDetector {
  public:
   QrFeatureDetector();
+  virtual ~QrFeatureDetector();
 };
 
 class QrTracker : public Kalman2DTracker {

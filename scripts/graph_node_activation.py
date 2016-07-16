14c7b4caadfdfd999f1d0ddf974fb62df2c2b8c3
"Luke Fraser" <lukefrasera@gmail.com>
2016-07-16 14:51:41 -0700

    adding latex features to the node_graph features
    --HG--
    branch : collaborative

--- scripts/graph_node_activation.py
+++ scripts/graph_node_activation.py
@@ -11,6 +11,76 @@ import logging
 import fnmatch
 import csv
 import pickle
+import pdb
+from math import sqrt
+import matplotlib
+SPINE_COLOR = 'gray'
+
+
+def latexify(fig_width=None, fig_height=None, columns=2):
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    columns : {1, 2}
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    assert(columns in [1,2])
+
+    if fig_width is None:
+        fig_width = 3.39 if columns==1 else 6.9 # width in inches
+
+    if fig_height is None:
+        golden_mean = (sqrt(5)-1.0)/2.0    # Aesthetic ratio
+        fig_height = fig_width*golden_mean # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + fig_height + 
+              "so will reduce to" + MAX_HEIGHT_INCHES + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    params = {'backend': 'ps',
+              'text.latex.preamble': ['\usepackage{gensymb}'],
+              'axes.labelsize': 8, # fontsize for x and y labels (was 10)
+              'axes.titlesize': 8,
+              'text.fontsize': 8, # was 10
+              'legend.fontsize': 8, # was 10
+              'xtick.labelsize': 8,
+              'ytick.labelsize': 8,
+              'text.usetex': True,
+              'figure.figsize': [fig_width,fig_height],
+              'font.family': 'serif'
+    }
+
+    matplotlib.rcParams.update(params)
+
+
+def format_axes(ax):
+
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_color(SPINE_COLOR)
+        ax.spines[spine].set_linewidth(0.5)
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_tick_params(direction='out', color=SPINE_COLOR)
+
+    return ax
+
 
 
 state_color_map = {'000' : [0.8, 0.9, 0.9, 1.0],
@@ -65,7 +135,8 @@ def GenerateHorizontalBar(data):
 
 def GraphData(data_hash, order):
     #update state color map
-    plt.figure(figsize=(16, 9e))
+    # plt.figure(figsize=(16, 9))
+    latexify()
     viridis = plt.get_cmap('magma')
     cnorm = colors.Normalize(vmin=0, vmax=100)
     scalarmap = cmx.ScalarMappable(norm=cnorm, cmap=viridis)
@@ -117,13 +188,13 @@ def GraphData(data_hash, order):
 
     ylables = list()
     labels = {
-        'THEN_0_1_001_state_Data_.csv'  : 'THEN_0',
+        'THEN_0_1_001_state_Data_.csv'  : 'THEN\_0',
         'PLACE_3_1_002_state_Data_.csv' : 'PLACEMAT',
-        'AND_3_1_003_state_Data_.csv'   : 'AND_0',
-        'OR_3_1_004_state_Data_.csv'    : 'OR_0',
+        'AND_3_1_003_state_Data_.csv'   : 'AND\_0',
+        'OR_3_1_004_state_Data_.csv'    : 'OR\_0',
         'PLACE_3_1_005_state_Data_.csv' : 'SPOON',
         'PLACE_3_1_006_state_Data_.csv' : 'FORK',
-        'THEN_0_1_007_state_Data_.csv'  : 'THEN_1',
+        'THEN_0_1_007_state_Data_.csv'  : 'THEN\_1',
         'PLACE_3_1_008_state_Data_.csv' : 'KNIFE',
         'PLACE_3_1_009_state_Data_.csv' : 'WINEGLASS',
         'PLACE_3_1_010_state_Data_.csv' : 'CUP',
@@ -139,6 +210,8 @@ def GraphData(data_hash, order):
     plt.ylabel("Behaviors")
     plt.xlabel("t(s)")
     plt.tight_layout()
+    ax = plt.gca()
+    format_axes(ax)
     plt.show()
 
 
